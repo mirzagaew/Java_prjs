45289e7d04e5052c1028801a9a8c4cb4b3639cb8
mirzagaew <mirzagaew@gmail.com>
2021-05-26 11:23:21 +0200

    Hello to Maven

---
+++ src/main/java/helloJava/HelloObject.java
@@ -0,0 +1,9 @@
+package helloJava;
+
+public class HelloObject {
+	public String printHello() {
+		String saying = "Hello, Java!"; 
+		System.out.println(saying);
+		return saying;
+		}
+}
---
+++ src/test/java/helloJava/HelloObjectTest.java
@@ -0,0 +1,14 @@
+package helloJava;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class HelloObjectTest {
+
+	@Test
+	public void testLength() {
+		HelloObject helloObj = new HelloObject();
+		assertEquals(12, helloObj.printHello().length());
+	}
+	
+}
