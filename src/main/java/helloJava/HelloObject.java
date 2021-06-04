6d309ab0aa48b7435b76f2789d489df049f189d9
mirzagaew <mirzagaew@gmail.com>
2021-06-04 15:15:44 +0200

    hello java was fixed

--- src/main/java/helloJava/HelloObject.java
+++ src/main/java/helloJava/HelloObject.java
@@ -2,7 +2,7 @@ package helloJava;
 
 public class HelloObject {
 	public String printHello() {
-		String greeting = "Hello, Java"; 
+		String greeting = "Hello, Java!"; 
 		System.out.println(greeting);
 		return greeting;
 		} 
